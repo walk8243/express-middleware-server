c0803fc65f9abdcf9bf1ec8c879c82f9438aa69f
walk8243 <walk8243@gmail.com>
2019-06-23 00:32:34 +0900

    サーバーをクラスター化して立ち上げる

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,22 @@
+import cluster from 'cluster';
+import os from 'os';
+
+const numCPUs = os.cpus().length;
+
 import app from "./app";
 
-const server = app.listen(app.get("port"), () => {
-  console.log('App is running at http://localhost:%d', app.get("port"));
-});
+if(cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
 
-export default server;
+  for(let i=0; i<numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  app.listen(app.get("port"), () => {
+    console.log('App is running at http://localhost:%d in %d pid', app.get("port"), process.pid);
+  });
+}
