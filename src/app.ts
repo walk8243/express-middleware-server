3da18853f5d68c1ffca945466c731a941e171638
walk8243 <walk8243@gmail.com>
2019-06-23 00:16:15 +0900

    ルーター無し状態でExpressサーバーを作成

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('port', process.env.PORT || 3000);
+
+export default app;
