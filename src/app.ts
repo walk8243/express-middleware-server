0a8652931c6cfaf7c76008e3e8755edc45fa00cd
walk8243 <walk8243@gmail.com>
2019-06-23 17:38:36 +0900

    ルーター側からインストールされるように設定を変更

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import bodyParser from "body-parser";
 
+import router from './router';
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', process.env.PORT || 3000);
+
+app.use(router);
 
 export default app;
