cbf0647ab6f2df5b00b7040565ebaae1e2be1aab
walk8243 <walk8243@gmail.com>
2019-06-28 19:47:20 +0900

    ドキュメント追加

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # express-middleware-server
 Expressのサーバー部のみ
+
+## 起動スクリプト
+- 場所
+  - **bin/server**
+- `yarn`でのインストール先からの実行方法
+  - `yarn run server`
+
+## src内のそれぞれのファイルの役割
+- server.ts
+  - Expressサーバーの起動
+  - クラスター設定
+- app.ts
+  - サーバー共通のミドルウェアの設定
+- router.ts
+  - アプリケーション内のルーターを指定
--- src/app.ts
+++ src/app.ts
@@ -6,15 +6,16 @@ import router from './router';
 
 const app = express();
 
-// ルーター前に使用するミドルウェア
+/* ここから ルーター前に使用するミドルウェア */
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', process.env.PORT || process.env.npm_package_config_port || 3000);
+/* ここまで ルーター前に使用するミドルウェア */
 
 // ルーターを使用
 app.use(router);
 
-// ルーター後に使用するミドルウェア
+/* ここから ルーター後に使用するミドルウェア */
 app.use((req, res, next) => {
   res.sendStatus(404);
 });
@@ -22,5 +23,6 @@ app.use((err: Error, req: express.Request, res: express.Response, next: express.
   console.error(err.stack);
   res.sendStatus(500);
 });
+/* ここまで ルーター後に使用するミドルウェア */
 
 export default app;
