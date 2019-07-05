1ea349ceac110a6ece0551d9b4f7f99a51a73b9b
walk8243 <walk8243@gmail.com>
2019-07-06 01:22:39 +0900

    serverのテストを作成

--- src/router.ts
+++ src/router.ts
@@ -2,7 +2,16 @@ import express from 'express';
 import * as path from 'path';
 import * as fs from 'fs';
 
-const appRouter = path.resolve(process.cwd(), process.env.ROUTER || process.env.npm_package_config_router || 'dist/router.js');
-const router = fs.statSync(appRouter).isFile() ? require(appRouter).default : express.Router();
+let router!: express.Router;
+if(process.env.ROUTER == 'false' || process.env.npm_package_config_router == 'false') {
+  router = express.Router();
+} else {
+  const appRouter = path.resolve(process.cwd(), process.env.ROUTER || process.env.npm_package_config_router || 'dist/router.js');
+  if(fs.statSync(appRouter).isFile()) {
+    router = require(appRouter).default;
+  } else {
+    throw new Error(`${appRouter} is not exists!`);
+  }
+}
 
 export default router;
