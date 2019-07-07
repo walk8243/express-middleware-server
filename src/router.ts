db9c87579336b4d2459e0c5a657d56416a280eaa
walk8243 <walk8243@gmail.com>
2019-07-07 20:28:35 +0900

    サーバー部用の環境変数を用意

--- src/router.ts
+++ src/router.ts
@@ -3,7 +3,7 @@ import * as path from 'path';
 import * as fs from 'fs';
 
 let router!: express.Router;
-if(process.env.ROUTER == 'false' || process.env.npm_package_config_router == 'false') {
+if(process.env.SERVER == 'true' || process.env.ROUTER == 'false' || process.env.npm_package_config_router == 'false') {
   router = express.Router();
 } else {
   const appRouter = path.resolve(process.cwd(), process.env.ROUTER || process.env.npm_package_config_router || 'dist/router.js');
